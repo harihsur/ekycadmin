dfe4d406ba10f3ffce47bf5b79165ac61fb42942
Suresh <harihsur@3c063002dbff.ant.amazon.com>
2022-11-15 14:47:29 +0800

    init commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "ekycadmin03b37a89": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
